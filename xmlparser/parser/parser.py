fa9d4900823712a2ab0d7aa9b413cc2e3d876d08
Matvei Vargasov <matveyvarg@gmail.com>
2021-02-05 12:40:42 +0300

    parse

--- xmlparser/parser/parser.py
+++ xmlparser/parser/parser.py
@@ -22,15 +22,35 @@ class XMLParser(IBaseParser):
     Parser for XML files
     """
 
-    def __init__(self, fields_to_find: Iterable[str])
-        self.
+    def __init__(self, fields_to_find: Iterable[str]):
+        self.fields_to_find = []
+        self.attrs_to_find = []
 
-    def parse_file(self, path: Union[str, Path]):
+        for field in fields_to_find:
+            if '.' in field:
+                self.attrs_to_find[tuple(field.split('.'))] = None
+            else:
+                self.fields_to_find[field] = None
+
+    def checkNode(self, node: Element):
+        """
+        Check if it's the node we are looking for
+        """
+        if node.tag in self.fields_to_find:
+            self.fields_to_find[node.tag] = node.text
+        else:
+            for tag, attr in self.attrs_to_find.keys():
+                if node.tag == tag:
+                    self.attrs_to_find[(tag, attr)] = node.attrib.get(attr)
+
+    def parse_file(self, path: Union[str, Path]) -> dict:
         """
         Parse given file
         """
 
         root = ET.parse(path)
 
+        for elem in root.iter():
+            self.checkNode(iter)
 
-        
+        return dict(**self.fields_to_find, **self.attrs_to_find)
